eb313a5176e33f4b961c1e173ba16000fff6eb75
Monika Ronowska <ronowska.monika@gmail.com>
2020-10-12 19:58:02 +0200

    operacje na liczbach

---
+++ zadCwiczenia.py
@@ -0,0 +1,33 @@
+imie, nazwisko, wiek = "Monix", "Banan", 24
+print(imie)
+print(nazwisko)
+print(wiek)
+
+print(type(nazwisko))
+
+print("Monix " + nazwisko)
+
+#f-string - formatowany string
+print(f"Twoje imię to {imie}, a nazwisko to {nazwisko}")
+
+#liczby_całkowite
+
+a = 6
+b = 19
+
+wynik = a + b
+print(wynik)
+
+wynik1 = a - b
+print(wynik1)
+
+wynik2 = a * b
+print(wynik2)
+
+wynik3 = a / b
+print(wynik3)
+
+wynik4 = a // b
+print(wynik4)
+
+print (30 < 32)
