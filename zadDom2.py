260eba803a165cff08edce34b99b7a820841b32a
Monika Ronowska <ronowska.monika@gmail.com>
2020-10-12 19:11:13 +0200

    dodanie zadania

---
+++ zadDom2.py
@@ -0,0 +1,11 @@
+print(
+    "In my place, in my place\nWere lines that I couldn't change\nI was lost, oh yeah\nI was lost, I was lost\nCrossed lines I shouldn't have crossed\ni was lost, oh yeah")
+
+print('''
+In my place, in my place
+Were lines that I couldn't change
+I was lost, oh yeah
+I was lost, I was lost
+Crossed lines I shouldn't have crossed
+i was lost, oh yeah
+''')
