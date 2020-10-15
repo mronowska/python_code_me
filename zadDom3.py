27cb19baf5c0fbe474949a8f15044a19e3719942
Monika Ronowska <monronow@gmail.com>
2020-10-15 17:16:12 +0200

    operacje na liczbach

---
+++ zadDom3.py
@@ -0,0 +1,9 @@
+name = "Andrzej"
+feature_positive = "wesoły"
+feature_negative = "chciwy"
+day_of_the_week = "czwartek"
+place = "wieś"
+animal = "słonia"
+
+print(
+    f"Był sobie raz człowiek o imieniu {name}. Z jednej strony {feature_positive}, ale z drugiej trochę {feature_negative}.\nPewnego dnia, chyba w {day_of_the_week}, to, że był {feature_negative} wprowadziło go w kłopoty. Gdy szedł przez {place}, napotkał na swojej drodze {animal}, który zjadał ludzi o tej cesze.")
