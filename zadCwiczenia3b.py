d802e94071c013591ae8352e790ca93633b19b00
Monika Ronowska <monronow@gmail.com>
2020-10-19 19:47:07 +0200

    zadanie na zajęciach

--- zadCwiczenia3b.py
+++ zadCwiczenia3b.py
@@ -13,3 +13,18 @@ print(osoba["imię"])
 # wyświetlanie wszystkich kluczy słownika
 print(osoba.keys())
 
+print("------------------")
+## iterowanie po słownikach
+
+# wartości (same klucze to i)
+for i in osoba:
+    print(osoba[i])
+
+# klucz i wartość
+for i in osoba:
+    print (f"{i} => {osoba[i]}")
+
+# przez items
+for klucz, wartosc in osoba.items():
+    print(f"{klucz} => {wartosc}")
+
