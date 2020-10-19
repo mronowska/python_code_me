d5014a497765bec1d2b644bbaeb9acdd736d43d2
Monika Ronowska <monronow@gmail.com>
2020-10-19 19:03:42 +0200

    zadania na zajęciach

---
+++ zad4.py
@@ -0,0 +1,9 @@
+pieski = ["wilczarz irlandzki", "buldog angielski", "owczarek podhalański", "terier szkocki", "dalmatyńczyk", "berneński pies pasterski", "grzywacz chiński"]
+
+for x in pieski:
+    print(x.upper())
+
+pieski.sort()
+print(pieski)
+
+
---
+++ zad5.py
@@ -0,0 +1,7 @@
+pierwsza_liczba = input("Podaj pierwszą liczbę: ")
+druga_liczba = input("Podaj drugą liczbę: ")
+trzecia_liczba = input("Podaj trzecią liczbę: ")
+
+lista = [pierwsza_liczba, druga_liczba, trzecia_liczba]
+
+print("".join(lista))
