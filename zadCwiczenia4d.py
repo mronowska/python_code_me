7741ac76dd6d7e2a2c5dd23345565bce6482b1c1
Monika Ronowska <monronow@gmail.com>
2020-10-22 19:58:15 +0200

    zadania na zajęciach

---
+++ zadCwiczenia4d.py
@@ -0,0 +1,14 @@
+# zadanie 4
+
+wynik = int(input("Podaj swój wynik w %: "))
+
+if wynik < 100 and wynik > 91:
+    print("5 - super")
+elif wynik < 90 and wynik > 75:
+    print("4 - oks")
+elif wynik < 74 and wynik > 51:
+    print("3 - średnio")
+elif wynik < 50 and wynik > 30:
+    print("2 - słabo")
+else:
+    print("Nie zdałeś.")
---
+++ zadCwiczenia4e.py
@@ -0,0 +1,9 @@
+#modyfikatory
+
+lista = ["Andrzej", "Monika", "Karolina", "Ania"]
+
+for imie in lista:
+    continue
+    print("A")
+    print("B")
+    print("C")
