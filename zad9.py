68a107d40d039fbc40ef27716feb14ab9de26888
Monika Ronowska <monronow@gmail.com>
2020-10-26 18:41:39 +0100

    modyfikacja o liczbe prob

--- zad9.py
+++ zad9.py
@@ -1,12 +1,15 @@
 #zgadywanie wylosowanej liczby
 
+
 import random
 wylosowana_liczba = random.randint(1, 101)
 print(f"Nie mów nikomu, ale wylosowana liczba to: {wylosowana_liczba}")
 
-liczba = int(input("Podaj liczbę: "))
+ile_prob = 6
+proba = 1
+liczba = int(input(f"Podaj liczbę. Uważaj, masz tylko {ile_prob} prób: "))
 
-while True:
+while proba < ile_prob:
     if liczba == wylosowana_liczba:
         print("WOW!! Wygrałeś/aś!")
         break
@@ -15,5 +18,6 @@ while True:
     if liczba > wylosowana_liczba:
         print("Podana liczba jest za duża, spróbuj ponownie!")
     liczba = int(input("Podaj liczbę: "))
+    proba += 1
 
 
