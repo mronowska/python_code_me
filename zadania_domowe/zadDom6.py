5d2e5d363fe326e60151be8807d7ade41d9a90e8
Monika Ronowska <monronow@gmail.com>
2020-11-01 22:23:24 +0100

    zmiana folderów

--- zadania_domowe/zadDom6.py
+++ zadania_domowe/zadDom6.py
@@ -8,10 +8,10 @@ def sprawdz_czy_liczba(wiek):
     return wiek.lstrip("-").isnumeric() #lstrip - usun z lewej znak z nawiasu, isnumeric - czy liczba
 
 def sprawdz_czy_liczba_ujemna(wiek):
-    pass
+    return wiek < 0
 
-def sprawdz_czy_liczba_parzysta():
-    pass
+def sprawdz_czy_liczba_parzysta(wiek):
+    return wiek % 2 == 0
 
 def wiek_mniejszy_od_0(wiek):
     if wiek < 0:
@@ -25,8 +25,18 @@ def main():
     wiek_napis = pobierz_dane()
     if sprawdz_czy_liczba(wiek_napis):
         print("Jest to liczba")
+        wiek = int(wiek_napis)
+        if sprawdz_czy_liczba_ujemna(wiek):
+            print("Liczba ujemna")
+        else:
+            print("Liczba dodatnia")
+        if sprawdz_czy_liczba_parzysta(wiek):
+            print("Liczba parzysta!")
+        else:
+            print("Liczba nieparzysta...")
     else:
         print("Nie jest to liczba")
 
 
 
+main()
