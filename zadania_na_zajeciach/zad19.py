685f0ca106bbee11edb2e75b084c6c43e6f44612
Monika Ronowska <monronow@gmail.com>
2020-11-02 19:57:31 +0100

    wyszukanie max liczby

--- zadania_na_zajeciach/zad19.py
+++ zadania_na_zajeciach/zad19.py
@@ -1,6 +1,14 @@
-def numbers():
-    number = []
-    number = input("Podaj liczby: ")
-    print(number)
+def numbers_function():
+    numbers = []
+    numbers = input("Podaj liczby: ")
+    numbers = numbers.split(", ")
+    numbers = [int(num) for num in numbers] # tworze liste int numbers, która jest pusta i dla dodaj po kolei elementy w formie int
+    ## to powyżej to to samo co:
+    # int_numbers = []
+    # for num in numbers:
+    #   int_numbers.append(int(num))
 
-numbers()
+    print(max(numbers))
+
+
+numbers_function()
