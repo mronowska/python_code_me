1bb75ab68008b2abc134447d5c2961f4fe08ab41
Monika Ronowska <monronow@gmail.com>
2020-11-09 18:42:42 +0100

    zadania na zajeciach

---
+++ zajecia/zadCwiczeniaCollections.py
@@ -0,0 +1,4 @@
+import collections
+
+c = collections.Counter("Piessski są super meeeega")
+print(c.most_common()) #liczy ile jest poszczegolnych znaków. Z argumentem liczy np 3 najpopularniejsze
---
+++ zajecia/zadCwiczenia_unittest.py
@@ -0,0 +1,35 @@
+import unittest
+
+def is_valid_number(number_string: str):
+    if number_string.lstrip("-").isdigit():
+        return True
+    else:
+        return False
+
+
+print(is_valid_number("12aaa"))
+
+class TestIsValidNumber(unittest.TestCase):
+
+    def test_valid_number(self):
+        #self.assertEqual('foo'.upper(), 'FOO') #sprawdz czy cos jest rownowazne
+        #inaczej
+        expected = True
+        actual = is_valid_number("12")
+        self.assertEqual(actual, expected)
+
+
+    def test_negative_number(self):
+        expected = True
+        actual = is_valid_number("-2137")
+        self.assertEqual(actual, expected)
+
+    def test_all_letters(self):
+        expected = False
+        actual = is_valid_number("absjskdf")
+        self.assertEqual(actual, expected)
+
+    def test_empty_string(self):
+        expected = False
+        actual = is_valid_number("")
+        self.assertEqual(actual, expected)
