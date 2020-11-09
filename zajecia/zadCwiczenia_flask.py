90757abe1d5597324a413f545c48d64587d4f939
Monika Ronowska <monronow@gmail.com>
2020-11-09 19:30:19 +0100

    dodanie zadania

--- zajecia/zadCwiczenia_flask.py
+++ zajecia/zadCwiczenia_flask.py
@@ -1,1 +1,12 @@
-import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+    #dowolny kod Python
+    #u nas wystawione na localhost
+    return "Hello World!"
+
+app.run()
