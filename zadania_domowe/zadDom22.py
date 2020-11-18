b85df4a49884de19c8ba3f1e54be738494028d93
Monika <monronow@gmail.com>
2020-11-18 21:31:58 +0100

    zamkniecie pliku

--- zadania_domowe/zadDom22.py
+++ zadania_domowe/zadDom22.py
@@ -7,12 +7,16 @@ def pobierz_dane():
 def odczytaj_plik():
     filepath = "pan_tadeusz.txt"
     f = open(filepath, "r", encoding="utf-8")
-    return f.read()
+    return f
 
 def policz_litery(sentence, num):
     c = collections.Counter(sentence)
     print(f"Najpopularniejsze znaki to: {c.most_common(num)}")
 
+def zamknij_tadeusza(file):
+    return file.close()
+
 tekst = pobierz_dane()
 policz_litery(tekst, len(tekst))
-policz_litery(odczytaj_plik(), 4)
+policz_litery(odczytaj_plik().read(), 4)
+zamknij_tadeusza(odczytaj_plik())
