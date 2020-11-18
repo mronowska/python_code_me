7a7b34f8b74a5071584cbf67c6f57560bf703703
Monika <monronow@gmail.com>
2020-11-18 21:57:59 +0100

    dodanie funkcji

--- zadania_domowe/zadDom17.py
+++ zadania_domowe/zadDom17.py
@@ -1,6 +1,9 @@
 import random
 
-a, b, c = input("Podaj liczby: ").split(',')
+def licz():
+    dolny_zakres, gorny_zakres, ilosc_liczb = input("Podaj liczby w formacie 'dolny zakres, gorny zakres, ilosc liczb' np. '2, 13, 5': ").split(', ')
 
-for i in range(0, int(c)):
-    print(random.randint(int(a), int(b)))
+    for i in range(0, int(ilosc_liczb)):
+        print(random.randint(int(dolny_zakres), int(gorny_zakres)))
+
+licz()
